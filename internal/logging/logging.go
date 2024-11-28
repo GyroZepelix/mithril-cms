28478398abe8158ab4fcf3440ac247ed3fcc9cb3
GyroZepelix <gamingskullgamer@gmail.com>
2024-11-28 18:44:03 +0100

    Fixed wrong file tracing in logging

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,8 +1,11 @@
 package logging
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"path/filepath"
+	"runtime"
 )
 
 type aggregatedLogger struct {
@@ -14,34 +17,54 @@ type aggregatedLogger struct {
 var al *aggregatedLogger
 
 func Init(out io.Writer) {
-	flags := log.LstdFlags | log.Lshortfile | log.Lmsgprefix
+	flags := log.LstdFlags
 	al = &aggregatedLogger{
-		infoLogger:  log.New(out, colorWhite("INFO: "), flags),
-		warnLogger:  log.New(out, colorYellow("WARN: "), flags),
-		errorLogger: log.New(out, colorRed("ERROR: "), flags),
+		infoLogger:  log.New(out, "", flags),
+		warnLogger:  log.New(out, "", flags),
+		errorLogger: log.New(out, "", flags),
+	}
+}
+
+func logWithCaller(logger *log.Logger, prefix string, v ...interface{}) {
+	_, file, line, ok := runtime.Caller(2)
+	if ok {
+		shortFile := filepath.Base(file)
+		logger.Println(fmt.Sprintf("%s:%d %s", shortFile, line, prefix), fmt.Sprint(v...))
+	} else {
+		logger.Println(fmt.Sprint(v...))
+	}
+}
+
+func logfWithCaller(logger *log.Logger, prefix, format string, v ...interface{}) {
+	_, file, line, ok := runtime.Caller(2)
+	if ok {
+		shortFile := filepath.Base(file)
+		logger.Printf(fmt.Sprintf("%s:%d %s %s", shortFile, line, prefix, format), v...)
+	} else {
+		logger.Printf(format, v...)
 	}
 }
 
 func Info(v ...interface{}) {
-	al.infoLogger.Println(colorWhiteRange(v...))
+	logWithCaller(al.infoLogger, colorWhite("INFO:"), colorWhiteRange(v...))
 }
 
 func Warn(v ...interface{}) {
-	al.warnLogger.Println(colorYellowRange(v...))
+	logWithCaller(al.warnLogger, colorWhite("WARN:"), colorYellowRange(v...))
 }
 
 func Error(v ...interface{}) {
-	al.errorLogger.Println(colorRedRange(v...))
+	logWithCaller(al.errorLogger, colorWhite("ERROR:"), colorRedRange(v...))
 }
 
 func Infof(format string, v ...any) {
-	al.infoLogger.Printf(colorWhite(format), v...)
+	logfWithCaller(al.infoLogger, colorWhite("INFO:"), colorWhite(format), v)
 }
 
 func Warnf(format string, v ...any) {
-	al.warnLogger.Printf(colorYellow(format), v...)
+	logfWithCaller(al.warnLogger, colorWhite("WARN:"), colorYellow(format), v)
 }
 
 func Errorf(format string, v ...any) {
-	al.errorLogger.Printf(colorRed(format), v...)
+	logfWithCaller(al.errorLogger, colorWhite("ERROR:"), colorRed(format), v)
 }
